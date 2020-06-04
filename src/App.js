30bc010d4d7269b756f4c9f18b5c6c174abbf533
Michael <michael_parvis@hotmail.com>
2020-06-04 15:56:36 +0100

    task complete, can be refactored to be cleaner

--- src/App.js
+++ src/App.js
@@ -6,7 +6,50 @@ import Score from './components/Score';
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    const questionArray = this.newQuestion();
+    this.state = {
+      correctAnswer: 0,
+      noQuestions: 0,            
+      value1:questionArray[0],
+      value2:questionArray[1],
+      value3:questionArray[2],
+      proposedAnswer: questionArray[3],
+    };
+  }
+
+  handleAnswer = (answer) => {
+    const { correctAnswer, noQuestions, proposedAnswer, value1, value2, value3} = this.state;
+      if ((proposedAnswer === value1+value2+value3) === answer) {
+        this.setState({correctAnswer: correctAnswer + 1});
+      }
+        this.setState({noQuestions: noQuestions + 1});
+        this.updateStateForNextQuestion();
+    }
+
+  randomNumber = () => Math.floor(Math.random() * 100);
+
+  newQuestion = () =>{
+    const value1 = this.randomNumber();
+    const value2 = this.randomNumber();
+    const value3 = this.randomNumber();
+    const proposedAnswer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
+    return [value1, value2, value3, proposedAnswer];
+  };
+  
+  updateStateForNextQuestion = () => {
+    const newQuestionArray = this.newQuestion();
+    this.setState({
+      value1:newQuestionArray[0],
+      value2:newQuestionArray[1],
+      value3:newQuestionArray[2],
+      proposedAnswer:newQuestionArray[3],
+    })
+  }
+    
   render() {
+    const { correctAnswer, noQuestions, proposedAnswer, value1, value2, value3} = this.state;
     return (
       <div className="App">
         <header className="App-header">
@@ -15,8 +58,17 @@ class App extends Component {
         </header>
         <div className="game">
           <h2>Mental Math</h2>
-            <Question />
-            <Score />
+            <Question
+             value1={value1}
+             value2={value2}
+             value3={value3}
+             proposedAnswer={proposedAnswer}
+             />
+            <Score 
+            handleAnswer={this.handleAnswer}
+            correctAnswer={correctAnswer}
+            noQuestions={noQuestions} 
+            />
         </div>
       </div>
     );
--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -1,31 +1,15 @@
 import React, { Component } from 'react';
 
 class Score extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          correctAnswer: 0,
-          noQuestions: 0,
-        };
-      }
-      
-      handleAnswer = (answer) => {
-        const { correctAnswer, noQuestions, proposedAnswer, value1, value2, value3} = this.state;
-          if ((proposedAnswer === value1+value2+value3) === answer) {
-            this.setState({correctAnswer: correctAnswer + 1});
-          }
-            this.setState({noQuestions: noQuestions + 1});
-      
-            this.updateStateForNextQuestion();
-        }
+
 
     render() {
         return (
             <div>
-                <button onClick={() => this.handleAnswer(true)}>True</button>
-                <button onClick={() => this.handleAnswer(false)}>False</button>
+                <button onClick={() => this.props.handleAnswer(true)}>True</button>
+                <button onClick={() => this.props.handleAnswer(false)}>False</button>
                 <p className="text">
-                Your Score: {this.state.correctAnswer}/{this.state.noQuestions}
+                Your Score: {this.props.correctAnswer}/{this.props.noQuestions}
                 </p>
             </div>
         );
